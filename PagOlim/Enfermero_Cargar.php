ed630bb3465fe17917e79ee4222441ab42263544
xRapstyle <118476674+xRapstyle@users.noreply.github.com>
2022-11-17 19:09:44 -0300

    Add files via upload

---
+++ PagOlim/Enfermero_Cargar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS.css">
+    <title>Carga del Enfermeros.</title>
+</head>
+<body>
+<div id="contenido">
+        <form action="" method="POST" class="register">
+        <h2>Cargar Enfermeros.</h2>
+            <div class="input-line">
+                <label for="name" class="label-form">Nombre</label>
+                <input type="text" name="name" id="name" class="input-field">
+            </div>
+            <div class="input-line">
+                <label for="surname" class="label-form">Apellido</label>
+                <input type="text" name="surname" id="surname" class="input-field">
+            </div>
+            <button type='submit' name='turnos' class="button-form">Completar Formulario.</button>
+</div>
+</body>
+</html> 
